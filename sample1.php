5caf705a5b5832383cd2d1a9e510078636eb56c7
Pierre Sylvestre <pierre.s@onthegosystems.com>
2023-05-24 08:43:57 -0300

    Wrap sample1 functions in a class for clarity

--- sample1.php
+++ sample1.php
@@ -1,36 +1,38 @@
 <?php
 
-/**
- * @param int $id
- *
- * @return array|null
- */
-function getItems( $id ) {
-	if ( $id ) {
-		$items = get_post_meta( $id, 'the_key' );
+class Foo {
+	/**
+	 * @param int $id
+	 *
+	 * @return array|null
+	 */
+	private function getItems( $id ) {
+		if ( $id ) {
+			$items = get_post_meta( $id, 'the_key' );
 
-		return is_array( $items ) ? $items : [];
-	}
-
-	return null;
-}
-
-function convert($id) {
-	$items = getItems( $id );
-
-	if ( $items ) {
-		foreach ( $items as $item ) {
-			convertItem( $item );
+			return is_array( $items ) ? $items : [];
 		}
+
+		return null;
 	}
 
-	return $items;
-}
+	public function convert($id) {
+		$items = $this->getItems( $id );
 
-function upgrade( $id ) {
-	$items = getItems( $id );
+		if ( $items ) {
+			foreach ( $items as $item ) {
+				convertItem( $item );
+			}
+		}
 
-	foreach ( $items as $item ) {
-		upgradeItem( $item );
+		return $items;
+	}
+
+	function upgrade( $id ) {
+		$items = $this->getItems( $id );
+
+		foreach ( $items as $item ) {
+			upgradeItem( $item );
+		}
 	}
 }
